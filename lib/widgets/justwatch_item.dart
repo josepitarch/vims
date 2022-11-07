5922ad70637b429e472bda7cd16a0f49d1d06572
jopimi <josepitarch98@gmail.com>
2022-11-07 22:22:56 +0100

    Add redirect platforms

--- lib/widgets/justwatch_item.dart
+++ lib/widgets/justwatch_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scrapper_filmaffinity/models/movie.dart';
 import 'package:scrapper_filmaffinity/utils/justwatch.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class JustwatchItem extends StatelessWidget {
   final Platform platform;
@@ -16,22 +17,28 @@ class JustwatchItem extends StatelessWidget {
         asset = key;
       }
     });
-    //TODO: redirect to platform
+    //TODO: Refactor in button
     return asset.isNotEmpty
-        ? Container(
-            height: 60,
-            margin: const EdgeInsets.symmetric(horizontal: 5),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.asset(
-                'assets/justwatch/$asset.jpg',
-                fit: BoxFit.cover,
-                height: 50,
-                width: 50,
-                errorBuilder: (_, __, ___) => const SizedBox(),
+        ? GestureDetector(
+            onTap: (() {
+              launchUrlString(platform.url,
+                  mode: LaunchMode.externalApplication);
+            }),
+            child: Container(
+              height: 60,
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  'assets/justwatch/$asset.jpg',
+                  fit: BoxFit.cover,
+                  height: 50,
+                  width: 50,
+                  errorBuilder: (_, __, ___) => const SizedBox(),
+                ),
               ),
             ),
           )
-        : const SizedBox();
+        : Text(platform.name);
   }
 }
