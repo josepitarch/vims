2d17543ca64e409a7924b3567214ddebe91647a4
jopimi <josepitarch98@gmail.com>
2023-05-26 19:22:03 +0200

    perf: improve search movie placeholder

--- lib/ui/input_decoration.dart
+++ lib/ui/input_decoration.dart
@@ -1,14 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:vims/constants/ui/search_movie_placeholder.dart';
 
 import '../providers/implementation/search_movie_provider.dart';
 
 class InputDecorations {
   static InputDecoration searchMovieDecoration(AppLocalizations i18n,
       TextEditingController controller, SearchMovieProvider provider) {
+    final int length = SEARCH_MOVIES_PLACEHOLDER.length;
+    final int random = Random().nextInt(length);
     return InputDecoration(
       prefixIcon: const Icon(Icons.search),
-      hintText: i18n.search_movie,
+      hintStyle: const TextStyle(fontStyle: FontStyle.italic),
+      hintText: SEARCH_MOVIES_PLACEHOLDER[random] + '...',
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(30.0),
         borderSide: const BorderSide(
