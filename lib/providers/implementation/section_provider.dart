cc0a570ae988c7fa361436ad183a728ddab0159b
jopimi <josepitarch98@gmail.com>
2023-12-17 22:29:30 +0100

    ref: rename file

---
+++ lib/providers/implementation/section_provider.dart
@@ -0,0 +1,38 @@
+import 'package:vims/models/section.dart';
+import 'package:vims/providers/interface/base_providert.dart';
+import 'package:vims/services/api/see_more_service.dart';
+
+class SectionProvider extends BaseProvider<Map<String, List<MovieSection>>> {
+  Map errors = {};
+  late String title;
+
+  SectionProvider() : super(data: {}, isLoading: true);
+
+  @override
+  fetchData() {
+    isLoading = true;
+    getSeeMore(title).then((movieSections) {
+      data![title] = movieSections;
+      errors[title] = null;
+    }).whenComplete(() {
+      isLoading = false;
+      notifyListeners();
+    });
+  }
+
+  fetchSection(String title) {
+    this.title = title;
+    fetchData();
+  }
+
+  @override
+  onRefresh() {
+    data!.clear();
+    errors.clear();
+  }
+
+  onRefreshError(String title) {
+    errors[title] = null;
+    notifyListeners();
+  }
+}
