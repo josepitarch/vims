f0d98e3a550b42e61f84141d7c6516f96a1ee9f1
jopimi <josepitarch98@gmail.com>
2023-12-20 22:38:49 +0100

    ref: add background to every review

--- lib/widgets/review_item.dart
+++ lib/widgets/review_item.dart
@@ -7,27 +7,33 @@ class ReviewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          review.body.replaceAll("\"", ''),
-          style: Theme.of(context).textTheme.bodyLarge,
-        ),
-        const SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              review.author,
-              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    fontStyle: FontStyle.italic,
-                  ),
-            ),
-            buildInclination(review.inclination)
-          ],
-        )
-      ],
+    return Container(
+      margin: const EdgeInsets.only(bottom: 12),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: Colors.grey[800]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            review.body.replaceAll("\"", ''),
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                review.author,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      fontStyle: FontStyle.italic,
+                    ),
+              ),
+              buildInclination(review.inclination)
+            ],
+          )
+        ],
+      ),
     );
   }
 
