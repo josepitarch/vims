2dc273ec870ac97eb742e2402a5d31479c1fca41
Jose Luis Pitarch Miravete <jpitarch@mercadona.es>
2024-03-07 08:51:02 +0100

    chore: don't redirect user to streaming platforms

--- lib/widgets/justwatch_item.dart
+++ lib/widgets/justwatch_item.dart
@@ -1,8 +1,7 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 import 'package:vims/models/enums/platforms.dart';
 import 'package:vims/models/movie.dart';
-import 'package:collection/collection.dart';
 
 class JustwatchItem extends StatelessWidget {
   final Platform platform;
@@ -14,23 +13,14 @@ class JustwatchItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    onTap() {
-      if (platform.url == null) return;
-      launchUrlString(platform.url!, mode: LaunchMode.externalApplication);
-    }
-
-    return InkWell(
-        highlightColor: Colors.transparent,
-        splashColor: Colors.transparent,
-        onTap: onTap,
-        child: Container(
-          height: height,
-          width: width,
-          margin: const EdgeInsets.symmetric(horizontal: 4),
-          child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: _Icon(platform, height: height, width: width)),
-        ));
+    return Container(
+      height: height,
+      width: width,
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: _Icon(platform, height: height, width: width)),
+    );
   }
 }
 
