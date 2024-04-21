f07ee00e319be44910ca42fdf8c263d4719f8d91
jopimi <josepitarch98@gmail.com>
2024-04-21 13:37:16 +0200

    chore: upgrade dependencies

--- lib/providers/implementation/bookmarks_provider.dart
+++ lib/providers/implementation/bookmarks_provider.dart
@@ -28,7 +28,6 @@ final class BookmarksProvider extends InfiniteScrollProvider<BookmarkMovie> {
                   exception = null,
                 })
             .catchError((e) {
-          print(e);
           exception = e;
         }).whenComplete(() {
           isLoading = false;
